16088cb41cc7d77d62fbc9099138f00b4adb27e1
IMeowable <151074908+IMeowable@users.noreply.github.com>
2023-11-30 17:11:10 +0300

    Update graphfunc.cpp

--- graphfunc/graphfunc.cpp
+++ graphfunc/graphfunc.cpp
@@ -1,211 +1,162 @@
-﻿using namespace std;
+using namespace std;
+#include <iostream>
+#include <sstream>
+#include <list>
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <list>
-string check_str;
-string check_last;
-string for_read;
 list<double> x_to_write;
 list<double> y_to_write;
-double curr_y;
 
-
-double an_argument;
 //Валидация вводимых значений, провеерка на принадлежность к вещественным числам
-bool isCorrectNum(string check_string)
-{
-	//Маркеры, показывающее, встречалась ли в строке буква или точка хотя бы раз
-	bool isPointer = false;
-	bool isSign = false;
 
-	//искоючаем наличие разделителя за пределами цифр числа
-	if (check_str[0] == '.' or check_str.back() == '.')
+bool IsDouble(string check_string)
+{
+
+	istringstream iss{ (check_string) };
+	double f;
+	iss >> noskipws >> f;
+	if (iss.eof() && !iss.fail() and f > -10)
 	{
+
+		return true;
+	}
+	else
+	{
+
 		return false;
 	}
-	//перебираем число на наличие точек между цифрами
-	for (int i = 1; i < check_str.size(); i++)
-	{
-		if (check_str[i] == '.')
-		{
-			if (isPointer == false)
-			{
-				if (!isdigit(check_str[0]) and !isdigit(check_str[1])) return false;
-
-				isPointer = true;// точка найдена, и она стоит гарантированно после числа
-				continue;
-			}
-			else return false;// если найдено больше 1 точки ,бракуем
-		}
-
-	}
-	for (int i = 0; i < check_str.size(); i++)
-	{
-
-		if (check_str[i] == '-')
-		{
-			if (isSign == false and check_str[0] == '-')
-			{
-				isSign = true;
-				continue;
-			}
-			else return false;
-
-		}//проверка есть ли на первой итерации знак минуса, если есть ставим маркер, и если в следующих итерациях встретится минус, возвращаем false
-
-
-	}
-	for (char a : check_string)
-	{
-		if (a == '.' or isdigit(a) or a == '-')
-		{
-			continue;
-		}
-		else
-		{
-			return false;
-		}
-		//проверяем нет ли в нашей строке посторонних символов
-	}
-	return true;
-	//Если после предыдущих проверок мы дошли до этой строчки, значит все условия соблюдены: минус, если есть, находится только в
-	//стоит только одна точка и она строго после целой части, в записи нет посторонних символов
 }
-//функция поиска точки на графике
 
+//функция поиска точки на графике
+double find_y(double user_input)
+
+{
+	double curr_y;
+
+	//определение принадлежности графику введенного значения условными операторами
+	if (user_input <= -6)
+	{
+		//уравнение полукруга y = -1 * sqrt(R^2 - x^2)
+		//r = 2, центр окружности (-8; 2)
+		//(x+8)^2+(y-2)^2 = 4 
+		//  (y-2)^2 = 4 - (x+8)^2
+		//y = sqrt(4-(x+8))^2) + 2
+		curr_y = -sqrt((4 - pow((user_input + 8), 2))) + 2;
+		cout << "Y:\t" << curr_y << endl;;
+	}
+	if (user_input > -6 && user_input <= -4)
+	{
+		//на графике фнукция параллельна оси Ox
+		curr_y = 2;
+		cout << "Y:\t" << curr_y << endl;;
+
+	}
+	if (user_input == 0) {
+		curr_y = 0;
+		cout << "Y:\t" << curr_y << endl;;
+	}
+	if (user_input > -4 && user_input < 2 && user_input != 0)
+	{
+		//k = -1/2 , функция y = kx + m	
+		curr_y = -user_input / 2.;
+		cout << "Y:\t" << curr_y << endl;;
+	}
+	if (user_input >= 2)
+	{
+		//функция y = kx + m    k = 1 m = -3
+		curr_y = user_input - 3.;
+		cout << "Y:\t" << curr_y << endl;;
+	}
+	return curr_y;
+}
 void txt_write()
 {
-	std::ofstream out("numbers.txt", std::ios::app);
+	std::ofstream out("numbers.txt", std::ios::app);//открываем в режиме записи в конец файла
 	if (out.is_open())
 	{
-		out << "x ";
-		for (auto i : x_to_write)
+		out << "x\t";
+		for (auto i : x_to_write)//перебираем наши иксы и отправляем в файл
 		{
-			out << i << " ";
+			out << i << "\t";
 
 		}
 		out << endl;
-		out << "y ";
-		for (auto i : y_to_write)
+		out << "Y\t";
+		for (auto i : y_to_write)//также перебираем Y
 		{
-			out << i << " ";
+			out << i << "\t";
 		}
 		out << endl;
 		out << endl;
 	}
 	out.close();
-
 }
+
+//В каждую итерацю добавил добавление текущих значений X и Y в соответствущие списки
+void cyclic_find()
+{
+	string check_first;
+	string check_second;
+	cin >> check_first;
+	cout << "Enter 'xn':\n";
+	cin >> check_second;
+	if (IsDouble(check_first) && IsDouble(check_second))
+	{
+		double i = stod(check_first);
+		cout << i<< endl;
+		double last = stod(check_second);
+		cout << last << endl;
+		if (i < last)
+		{
+			for (i; i <= last; i++)
+			{
+
+				cout << "X:\t"<< i<<"\t";
+				double to_write = find_y(i);
+				x_to_write.push_back(i);
+				y_to_write.push_back(to_write);
+				
+
+			}
+			txt_write();
+		}
+		else cout << "Enter numbers from left to the right" << endl;
+	}
+	else
+	{
+		cout << "Wrong, try again" << endl;
+	}
+}
+
+//При каждом запуске в консоль будет выводиться содержимое текстового файла, после ознакомления с ним - enter, консоль очистится, можно снова развлекаться с функцией.
 void txt_read()
 
 {
-	cout << "Previous calculations: (from txt file)\n";
-	cout<< endl;
-
-	std::ifstream in("numbers.txt"); // окрываем файл для чтения
+	
+	cout << "Previous calculations: (from txt file)";
+	cout << endl;
+	string line;
+	ifstream in("numbers.txt"); 
 	if (in.is_open())
 	{
-		while (std::getline(in, for_read))
+		while (std::getline(in, line))
 		{
-			std::cout << for_read << std::endl;
+			std::cout << line << std::endl;
 		}
 	}
-
 	in.close();     // закрываем файл
+	cout << "For next work press enter(and clean console)";
+	getchar();
+	system("cls");
 }
-
-void find_y() {
-	//Ввод значения с клавиатуры
-
-	cout << "Enter 'x0':\n";
-	cin >> check_str;
-	cout << "Enter 'xn':\n";
-	cin >> check_last;
-	
-	
-		if (isCorrectNum(check_str) && isCorrectNum(check_last) == true)
-		{
-			{
-				
-				an_argument = std::stod(check_str.substr());
-			
-				double edge_x = std::stod(check_last.substr());
-				while(an_argument <= edge_x)
-				{
-					//определение принадлежности графику введенного значения условными операторами
-					if (an_argument >= -10 && an_argument < -6)
-					{
-						//уравнение полукруга y = -1 * sqrt(R^2 - x^2)
-						//r = 2, центр окружности (-8; 2)
-						//(x+8)^2+(y-2)^2 = 4 
-						//  (y-2)^2 = 4 - (x+8)^2
-						//y = sqrt(4-(x+8))^2) + 2
-
-						double pre_compute = (4 - pow((an_argument + 8), 2));
-						double func_y = -sqrt(pre_compute) + 2;
-						cout << "Y:\n" << func_y << "\n";
-						curr_y = func_y;
-						
-
-					}
-					if (an_argument >= -6 && an_argument <= -4)
-					{
-						//на графике фнукция параллельна оси Ox
-						cout << 2 << "\n";
-						curr_y = 2;
-						
-					}
-					if (an_argument == 0) {
-						cout << 0 << "\n";
-						curr_y = 0;
-					}
-					if (an_argument > -4 && an_argument <= 2 && an_argument != 0)
-					{
-						//k = -1/2 , функция y = kx + m
-						double  result = -an_argument / 2.;
-
-						cout << result << "\n";
-						curr_y = result;
-						
-					}
-
-					if (an_argument > 2)
-					{
-						//функция y = kx + m k = 1 m = -3
-						double  result = an_argument - 3.;
-						cout << result << "\n";
-						curr_y = result;
-					}
-					if (an_argument < -10)
-					{
-						//проверка выходим ли за значения Х, установленные заданием
-						cout << "ERROR!\nTry other one\n";
-						
-
-					}
-					x_to_write.push_back(an_argument);
-					y_to_write.push_back(curr_y);
-					an_argument++;
-				}
-				
-			}
-			txt_write();
-			
-			find_y();
-			
-		}
-		else
-		{
-			cout << "Incorrect input\n";
-			find_y();
-		}
-	}
+ 
 
 
 int main()
 {
-	
 	txt_read();
-	find_y();
+	while (1)
+		
+		cyclic_find();
 }
